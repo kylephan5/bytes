649d37aa426f680c59923d9d3176ec7052944bd2
torimen1 <victoria.f.mendez02@gmail.com>
2023-10-31 17:37:50 -0400

    navbar

--- bytes_client/src/components/pages/Home.js
+++ bytes_client/src/components/pages/Home.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import '../../App.css';
+import MainSection from '../MainSection';
 
 function Home() {
   return (
     <>
+      <MainSection />
     </>
   )
 }
 
-export default Home;
+export default Home
