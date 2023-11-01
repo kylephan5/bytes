a66153033ff5bbc53b823bc9b9b92eaa85fe4b73
mayleenl <mayleen.liu@gmail.com>
2023-10-31 23:41:47 -0400

    add style and forgot button for login/signup

--- bytes_client/src/components/pages/Login.js
+++ bytes_client/src/components/pages/Login.js
@@ -2,64 +2,66 @@ import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import './Login.css';
 import axios from "axios"; // Import your CSS file
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 
 function Signup() {
-    const [formData, setFormData] = useState({
-        email: '',
-        password: '',
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+  })
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  }
+
+  const changePassword = (old_password, new_password, confirm_password) => {
+    axios.put('change_password/', {
+      old_password: old_password,
+      new_password: new_password,
+      confirm_password: confirm_password,
+    }).then(function (response) {
+      console.log(response);
+    }).catch(function (error) {
+      console.error(error);
     })
+  }
 
-    const handleInputChange = (event) => {
-        const {name, value} = event.target;
-        setFormData({
-            ...formData,
-            [name]: value,
-        });
-    }
+  const userLogin = (event) => {
+    event.preventDefault();
+    const { email, password } = formData;
 
-    const changePassword = (old_password, new_password, confirm_password) => {
-        axios.put('change_password/', {
-          old_password: old_password,
-          new_password: new_password,
-          confirm_password: confirm_password,
-      }).then(function(response) {
-          console.log(response);
-        }).catch(function(error) {
-          console.error(error);
-        })
-    }
-
-    const userLogin = (event) => {
-        event.preventDefault();
-        const {email , password} = formData;
-
-        axios.post('login/', {
-          email: email,
-          password: password,
-        }, {
-            withCredentials: true,
-        }).then(function(response) {
-          console.log('signed in');
-        }).catch(function(error) {
-          console.error(error);
-        })
-    }
+    axios.post('login/', {
+      email: email,
+      password: password,
+    }, {
+      withCredentials: true,
+    }).then(function (response) {
+      console.log('signed in');
+    }).catch(function (error) {
+      console.error(error);
+    })
+  }
 
   return (
     <Form className="login-form"> {/* Apply the login-form class */}
       <Form.Group controlId="formBasicEmail">
         <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" name="email" value={formData.email} onChange={handleInputChange}/>
+        <Form.Control type="email" placeholder="Enter email" name="email" value={formData.email} onChange={handleInputChange} />
       </Form.Group>
 
       <Form.Group controlId="formBasicPassword">
         <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" name="password" value={formData.password} onChange={handleInputChange}/>
+        <Form.Control type="password" placeholder="Password" name="password" value={formData.password} onChange={handleInputChange} />
       </Form.Group>
       <Button variant="primary" type="submit" className="login-button" onClick={(event) => userLogin(event)}> {/* Apply the login-button class */}
         Submit
       </Button>
+      <a href="#" className="forgot-password-button">Forgot Password?</a>
+
     </Form>
   );
 }
