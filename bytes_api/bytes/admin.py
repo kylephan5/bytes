2f5903f56a4b4db78a8793b179c7ab4cecf23cd1
kylephan <kyledphan@gmail.com>
2023-11-01 20:26:29 -0400

    imported kaggle dataset into database

--- bytes_api/bytes/admin.py
+++ bytes_api/bytes/admin.py
@@ -5,8 +5,6 @@ from .models import CustomUser
 from .models import *
 
 admin.site.register(CustomUser)
-
 admin.site.register(Inventory)
 admin.site.register(Recipe)
-admin.site.register(Recipe_Ingredient)
-admin.site.register(Temp_Table)
+admin.site.register(RecipeIngredient)
--- bytes_api/bytes/models.py
+++ bytes_api/bytes/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+from django.db.models import IntegerField
 
 # Create your models here.
 
@@ -53,63 +54,57 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
     def __str__(self):
         return self.email
 
+
 '''
 Inventory class: Used to store what items a specific user has
 email: 
-'''  
+'''
+
+
 class Inventory(models.Model):
     email = models.EmailField(unique=True)
     ingredient = models.CharField(max_length=75)
 
-    
-    #gluten_free_bool = models.BooleanField(default=False)
-    #vegitarian_bool = models.BooleanField(default=False)
-    #vegan_bool = models.BooleanField(default=False)
-    #lactose_bool = models.BooleanField(default=False)
-    #nut_free_bool = models.BooleanField(default=False)
-    #shellfish_free_bool = models.BooleanField(default=False)
+    # gluten_free_bool = models.BooleanField(default=False)
+    # vegitarian_bool = models.BooleanField(default=False)
+    # vegan_bool = models.BooleanField(default=False)
+    # lactose_bool = models.BooleanField(default=False)
+    # nut_free_bool = models.BooleanField(default=False)
+    # shellfish_free_bool = models.BooleanField(default=False)
 
     def __str__(self):
         return self.email
 
+
 '''
 Recipe class: Used to store recipe data
 '''
+
+
 class Recipe(models.Model):
-    recipe_index = models.IntegerField(default = 0)
-    recipe_name  = models.CharField(max_length=100)
-    recipe_url   = models.CharField(max_length=150)
-    gluten_free_bool = models.BooleanField(default=False)
-    vegitarian_bool = models.BooleanField(default=False)
-    vegan_bool = models.BooleanField(default=False)
-    lactose_bool = models.BooleanField(default=False)
-    nut_free_bool = models.BooleanField(default=False)
-    shellfish_free_bool = models.BooleanField(default=False)
+    recipe_id = models.AutoField(primary_key=True, default=None)
+    recipe_name = models.CharField(max_length=100)
+    recipe_url = models.CharField(max_length=150)
+    gluten_friendly = models.BooleanField(default=True)
+    vegan_friendly = models.BooleanField(default=True)
+    vegetarian_friendly = models.BooleanField(default=True)
+    lactose_friendly = models.BooleanField(default=True)
+    keto_friendly = models.BooleanField(default=True)
+    nut_friendly = models.BooleanField(default=True)
+    shellfish_friendly = models.BooleanField(default=True)
 
     def __str__(self):
-        return self.recipe_index
+        return self.recipe_name
 
- '''
- Recipe_Ingredient: Splits up the NER column of the kaggle data set, used as a join table for inventory and recipe
- '''   
-class Recipe_Ingredient(model.Models):
-    recipe_index = models.IntegerField(default = 0)
+
+class RecipeIngredient(models.Model):
+    recipe = models.ForeignKey(Recipe, default=0,
+                               on_delete=models.CASCADE)  # if a recipe is deleted, all of the items inside of recipe ingredient will be deleted
     recipe_ingredient = models.CharField(max_length=100)
 
     def __str__(self):
-        return self.recipe_index
-    
-'''
-Temporary table for stage 3 demo
-'''
-class Temp_Table(models.Model):
-    recipe_index = models.IntegerField(default = 0)
-    recipe_name  = models.CharField(max_length=100)
-    recipe_url   = models.CharField(max_length=150)
-    recipte_ingredient_list = models.CharField(max_length=500)
+        return self.recipe_ingredient
 
-    def __str__(self):
-        return self.recipe_index
 
 
 
