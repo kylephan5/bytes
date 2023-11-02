c49bbc8e268a518f5012aca515021c49d12d10ee
mayleenl <mayleen.liu@gmail.com>
2023-11-02 19:11:06 -0400

    add x to inventory

--- bytes_client/src/components/pages/Inventory.js
+++ bytes_client/src/components/pages/Inventory.js
@@ -1,5 +1,66 @@
+// import React, { useState } from 'react';
+// // import '../../App.css';
+// import './Inventory.css';
+
+// function Inventory() {
+//   const [images, setImages] = useState([]);
+//   const fileInputRef = React.createRef();
+
+//   const handleDrop = (e) => {
+//     e.preventDefault();
+//     const droppedFiles = Array.from(e.dataTransfer.files);
+//     setImages([...images, ...droppedFiles]);
+//   };
+
+//   const handleDragOver = (e) => {
+//     e.preventDefault();
+//   };
+
+//   const handleFileSelect = (e) => {
+//     const selectedFiles = Array.from(e.target.files);
+//     setImages([...images, ...selectedFiles]);
+//   };
+
+//   const handleUploadClick = () => {
+//     fileInputRef.current.click();
+//   };
+
+//   return (
+//     <div className="inventory-container">
+//       <div
+//         className="drop-area"
+//         onDrop={handleDrop}
+//         onDragOver={handleDragOver}
+//       >
+//         <p>Drag and drop images here</p>
+//         <button type="button" onClick={handleUploadClick}>
+//           Upload Images
+//         </button>
+//         <input
+//           type="file"
+//           multiple
+//           accept="image/*"
+//           style={{ display: 'none' }}
+//           onChange={handleFileSelect}
+//           ref={fileInputRef}
+//         />
+//       </div>
+//       <div className="image-preview">
+//         {images.map((image, index) => (
+//           <img
+//             key={index}
+//             src={URL.createObjectURL(image)}
+//             alt={`Image ${index}`}
+//           />
+//         ))}
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default Inventory;
+
 import React, { useState } from 'react';
-// import '../../App.css';
 import './Inventory.css';
 
 function Inventory() {
@@ -25,6 +86,12 @@ function Inventory() {
     fileInputRef.current.click();
   };
 
+  const handleRemoveImage = (index) => {
+    const updatedImages = [...images];
+    updatedImages.splice(index, 1);
+    setImages(updatedImages);
+  };
+
   return (
     <div className="inventory-container">
       <div
@@ -47,11 +114,18 @@ function Inventory() {
       </div>
       <div className="image-preview">
         {images.map((image, index) => (
-          <img
-            key={index}
-            src={URL.createObjectURL(image)}
-            alt={`Image ${index}`}
-          />
+          <div key={index} className="image-container">
+            <img
+              src={URL.createObjectURL(image)}
+              alt={`Image ${index}`}
+            />
+            <button
+              className="remove-button"
+              onClick={() => handleRemoveImage(index)}
+            >
+              X
+            </button>
+          </div>
         ))}
       </div>
     </div>
@@ -59,3 +133,4 @@ function Inventory() {
 }
 
 export default Inventory;
+
