30e67e0d85f7d52e89357cb4a4ce93508e16b460
mayleenl <mayleen.liu@gmail.com>
2023-11-01 20:35:30 -0400

    landing page format, navbar tab indicator

--- bytes_client/src/components/Navbar.js
+++ bytes_client/src/components/Navbar.js
@@ -1,9 +1,8 @@
 import React, { useState } from "react";
 import "./Navbar.css";
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 function Navbar() {
-
     const [Menuclick, setMenuclick] = useState(false);
     const toggleMenu = () => setMenuclick(!Menuclick);
     const closemenu = () => setMenuclick(false);
@@ -12,34 +11,38 @@ function Navbar() {
         <>
             <div className="navbar">
                 <div className="navbar-container">
-
                     <div className='menu-icon' onClick={toggleMenu}>
                         {Menuclick ? (<span className="material-symbols-outlined">close</span>)
                             : (<span className="material-symbols-outlined">menu</span>)}
                     </div>
                     <ul className={Menuclick ? 'nav-menu active' : 'nav-menu'}>
                         <li className='nav-item'>
-                            <Link to='/profile' className='nav-links' onClick={closemenu}>PROFILE</Link>
+                            <NavLink to='/profile' className='nav-links' activeClassName='active' onClick={closemenu}>
+                                PROFILE
+                            </NavLink>
                         </li>
                         <li className='nav-item'>
-                            <Link to='/' className='nav-links' onClick={closemenu}>HOME</Link>
-                        </li>
+                            <NavLink to='/' className='nav-links' activeClassName='active' exact onClick={closemenu}>
+                                HOME
+                            </NavLink>                        </li>
                         <li className='nav-item'>
-                            <Link to='/inventory' className='nav-links' onClick={closemenu}>INVENTORY</Link>
-                        </li>
+                            <NavLink to='/inventory' className='nav-links' activeClassName='active' onClick={closemenu}>
+                                INVENTORY
+                            </NavLink>                        </li>
                         <li className='nav-item'>
-                            <Link to='/recipes' className='nav-links' onClick={closemenu}>RECIPES</Link>
-                        </li>
+                            <NavLink to='/recipes' className='nav-links' activeClassName='active' onClick={closemenu}>
+                                RECIPES
+                            </NavLink>                        </li>
                         <li className='nav-item'>
-                            <Link to='/login' className='nav-links' onClick={closemenu}>LOGIN</Link>
-                        </li>
+                            <NavLink to='/login' className='nav-links' activeClassName='active' onClick={closemenu}>
+                                LOGIN
+                            </NavLink>                        </li>
                         {/* <li className='nav-item'>
                             <Link to='/signup' className='nav-links' onClick={closemenu}>SIGNUP</Link>
                         </li> */}
                     </ul>
                 </div>
             </div>
-
         </>
     )
 }
