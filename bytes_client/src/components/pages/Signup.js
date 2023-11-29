19674003b9444cead1955a23be390df4f3d739c1
mayleenl <mayleen.liu@gmail.com>
2023-11-29 18:11:33 -0500

    fix serializer issue, login/change pwd/signup edits

--- bytes_client/src/components/pages/Signup.js
+++ bytes_client/src/components/pages/Signup.js
@@ -1,110 +1,96 @@
+import React, { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import './Login.css';
-import axios from "axios"; // Import your CSS file
-import React, {useState} from 'react';
+import axios from 'axios';
 
 function Signup() {
-    const [formData, setFormData] = useState({
-        email: '',
-        password: '',
-    })
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+  });
+  const [registrationSuccess, setRegistrationSuccess] = useState(false);
 
-    const handleInputChange = (event) => {
-        const {name, value} = event.target;
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const userRegister = (event) => {
+    event.preventDefault();
+    const { email, password } = formData;
+
+    axios
+      .post('register/', {
+        email: email,
+        password: password,
+      })
+      .then(function (response) {
+        setRegistrationSuccess(true);
+
         setFormData({
-            ...formData,
-            [name]: value,
+          email: '',
+          password: '',
         });
-    }
 
-    // const sendRequest = () => {
-    //     axios.get('users/', {
-    //         format: 'json',
-    //     })
-    //     .then(function (response) {
-    //       console.log(response);
-    //     }).catch(function(response) {
-    //       console.log(response);
-    //     })
-    //   }
+        axios
+          .post('login/', {
+            email: email,
+            password: password,
+          })
+          .then(function (response) {
+            console.log('signed in!');
+          });
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  };
 
-      // sendUser
-      const userRegister = (event) => {
-        event.preventDefault();
-        const {email , password} = formData;
-
-        axios.post('register/', {
-          email: email,
-          password: password,
-        }).then(function(response){
-            // sign user in
-            axios.post('login/', {
-                email: email,
-                password:password,
-            }).then(function(response) {
-                console.log('signed in!');
-            })
-        }).catch(function(error) {
-            console.log(error);
-        })
-
-      }
-
-    // const getSpecific = (id) => {
-    //   axios.get(`users/`, {
-    //     params: { id: id },
-    //     headers: { 'Content-Type': 'application/json' } // You can specify the request headers here
-    //   })
-    //     .then(function (response) {
-    //       console.log(response);
-    //     })
-    //     .catch(function (error) {
-    //       console.error(error);
-    //     });
-    // }
-
-    // const changePassword = (old_password, new_password, confirm_password) => {
-    //     axios.put('change_password/', {
-    //       old_password: old_password,
-    //       new_password: new_password,
-    //       confirm_password: confirm_password,
-    //   }).then(function(response) {
-    //       console.log(response);
-    //     }).catch(function(error) {
-    //       console.error(error);
-    //     })
-    // }
-
-    // const userLogin = (event, email, password) => {
-    //     event.preventDefault();
-    //     axios.post('login/', {
-    //       email: email,
-    //       password: password,
-    //     }, {
-    //         withCredentials: true,
-    //     }).then(function(response) {
-    //       console.log(response);
-    //     }).catch(function(error) {
-    //       console.error(error);
-    //     })
-    // }
   return (
-    <Form className="login-form"> {/* Apply the login-form class */}
+    <Form className="login-form">
       <Form.Group controlId="formBasicEmail">
         <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" name="email" value={formData.email} onChange={handleInputChange}/>
+        <Form.Control
+          type="email"
+          placeholder="Enter email"
+          name="email"
+          value={formData.email}
+          onChange={handleInputChange}
+        />
       </Form.Group>
 
       <Form.Group controlId="formBasicPassword">
         <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" name="password" value={formData.password} onChange={handleInputChange}/>
+        <Form.Control
+          type="password"
+          placeholder="Password"
+          name="password"
+          value={formData.password}
+          onChange={handleInputChange}
+        />
       </Form.Group>
-      <Button variant="primary" type="submit" className="login-button" onClick={(event) => userRegister(event)}> {/* Apply the login-button class */}
+
+      <Button
+        variant="primary"
+        type="submit"
+        className="login-button"
+        onClick={(event) => userRegister(event)}
+      >
         Submit
       </Button>
+
+      {registrationSuccess && (
+        <div className="registration-success-message">
+          <p>
+            Registration successful! Please go to the login page.
+          </p>
+        </div>
+      )}
     </Form>
   );
 }
-
 export default Signup;
