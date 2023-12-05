8c3885ef66e0b263850998f17d6c377a91b70275
mayleenl <mayleen.liu@gmail.com>
2023-12-04 20:21:12 -0500

    remove search/filter from recommendations

--- bytes_client/src/components/pages/Recommendations.js
+++ bytes_client/src/components/pages/Recommendations.js
@@ -40,17 +40,6 @@ function Recommendations() {
     };
 
 
-    const handleFilterChange = (filterName) => {
-        const updatedFilters = {
-            ...filters,
-            [filterName]: !filters[filterName],
-        };
-        console.log('Updated Filters:', updatedFilters);
-        setFilters(updatedFilters);
-        fetchRecommendations({ ...updatedFilters, search: searchQuery });
-    };
-
-
     const handleVote = async (recipeId, value) => {
         try {
             const voteUrl = `recipes/${recipeId}/vote/`;
@@ -66,11 +55,6 @@ function Recommendations() {
             console.error('Error voting:', error);
         }
     };
-
-    const handleSearchChange = (e) => {
-        setSearchQuery(e.target.value);
-    };
-
     const renderRecipeGrid = () => {
         return (
             <div className="recipe-cards">
@@ -101,28 +85,6 @@ function Recommendations() {
         <div className="recipe-container">
             <h1>Recipe page</h1>
 
-            <input
-                type="text"
-                placeholder="Search recipes..."
-                value={searchQuery}
-                onChange={handleSearchChange}
-            />
-
-            <div className="filter-section">
-                <h3>Filters:</h3>
-                <div className='filter-boxes'>
-                    {Object.keys(filters).map((filterName) => (
-                        <label key={filterName}>
-                            <input
-                                type="checkbox"
-                                checked={filters[filterName]}
-                                onChange={() => handleFilterChange(filterName)}
-                            />
-                            {filterName.replace('Friendly', '').replace(/([A-Z])/g, ' $1').trim()}
-                        </label>
-                    ))}
-                </div>
-            </div>
 
             {renderRecipeGrid()}
         </div>
